e5738512119d2398d0c50d916b9760ea0a80856f
Sstee1XD <843462642@qq.com>
2020-09-22 15:45:40 +0800

    清单1 - 1

--- src/wuhu/qifei.java
+++ src/wuhu/qifei.java
@@ -3,8 +3,7 @@ package wuhu;
 public class qifei {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world!");
+		System.out.println("Welcome to Jave!");
 	}
 
 }
