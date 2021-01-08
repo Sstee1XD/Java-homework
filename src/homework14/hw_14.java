b1f82abfff83f96cfb601247f50e247411b3bd44
Sstee1XD <843462642@qq.com>
2021-01-08 14:32:09 +0800

    homework14

---
+++ src/homework14/hw_14.java
@@ -0,0 +1,31 @@
+package homework14;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class hw_14 {
+
+	public static void main(String[] args) {
+		InputStream in = null;
+		OutputStream out = null;
+		
+		try {
+			in = new FileInputStream(new File("src/homework14/input.txt"));
+			out = new FileOutputStream(new File("src/homework14/input.txt"));
+			
+			int bufferSize = 10;
+			byte[] buffer = new byte[bufferSize];
+			int len;
+			
+			while ((len = in.read(buffer)) > 0) {
+				out.write(buffer, 0, len);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
