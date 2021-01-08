4502f61f6ac6353fcc49ae68d4763d1c9831290a
Sstee1XD <843462642@qq.com>
2021-01-08 14:24:11 +0800

    homework2

---
+++ src/wuhu/hw2_8.java
@@ -0,0 +1,15 @@
+package wuhu;
+
+import java.util.Scanner;
+
+public class hw2_8 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter purchase amount: ");
+		double purchaseAmount = input.nextDouble();
+		double tax = purchaseAmount * 0.06;
+		System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);
+	}
+
+}
